c9946ffe81bbab92828c9dbe5abcda3c0c719095
Johanna Hilaire <johannahilairepro@gmail.com>
2023-06-11 10:14:44 +0200

    setup App structure and shuffle

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
 import './App.css'
+import { useEffect, useState } from 'react';
+
+
+
+const [cards,setCards] = useState([])
 
 function App() {
-  <div className="App"></div>
-  return (
-    <>
-     
-    </>
-  )
-}
+  const initGame = () => {
+    const cards = 
+      new Array(16)
+      .fill()
+      .map((_,i) => ({
+        src : `/pix/0${i%8+1}.jpg`,
+        revealed : false,  
+      }))
+      .sort(() => Math.random() -.5)
+    setCards(cards)
+  } 
+ 
+  useEffect(() => initGame() , [])
 
-export default App
+  return (
+      <div className="App">
+          {cards.map((card,i) => (
+            <div>
+              <img src={card.src}/>
+            </div>
+
+          ))}
+
+      </div>
+
+  )
+};
+
+export default App;
